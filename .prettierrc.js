95aed85881a970fcb4426ff9222dcada6cf911b0
Seba Beltran <sbeltran@unitedvirtualities.com>
2022-01-28 21:05:08 -0300

    Pages and routing

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: "all",
+  printWidth: 80,
+  useTabs: false,
+  semi: true,
+  jsxSingleQuote: true,
+};
---
+++ pages/events/add.tsx
@@ -0,0 +1,5 @@
+const AddEventsPage = () => {
+  return <h1>Add Events</h1>;
+};
+
+export default AddEventsPage;
